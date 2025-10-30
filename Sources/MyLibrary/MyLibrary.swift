3e930bd813bfa074561f919538fd9d232bd9aa8f
AmiraslanliAF <arkan.amiraslanli@kapitalbank.az>
2025-10-30 15:01:35 +0400

    hakku

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public final class TestClass {
+    public static func testMethod(message: String) {
+        print("MESSAGE \(message)")
+    }
+}
+
+
+public final class TestClass2 {
+    public static func testMethod(message: String) {
+        print("MESSAGE  2 \(message)")
+    }
+}
