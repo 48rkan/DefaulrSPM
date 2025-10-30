20bc6de5ccb043ac8695b46a048e2344e0a843aa
AmiraslanliAF <arkan.amiraslanli@kapitalbank.az>
2025-10-30 14:00:38 +0400

    push

---
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public final class TestClass {
+    public func testMethod(message: String) {
+        print("MESSAGE \(message)")
+    }
+}
