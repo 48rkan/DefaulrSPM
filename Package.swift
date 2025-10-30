9a837f951e49dc624e421a55866b7da5f0b11267
AmiraslanliAF <arkan.amiraslanli@kapitalbank.az>
2025-10-30 17:18:04 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DefaultSPM",
-            targets: ["DefaultSPM"]),
+            targets: ["MyTestFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
